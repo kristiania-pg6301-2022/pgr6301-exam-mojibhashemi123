8943c48650673b858d98eb5dd9be27f6d5793eeb
mojibhashemi123 <muha006@student.kristiania.no>
2022-05-06 03:53:05 +0200

    Test adds article on submit

--- client/__tests__/createNewArticle.test.jsx
+++ client/__tests__/createNewArticle.test.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { MemoryRouter } from "react-router-dom";
 import { CreateNewArticle } from "../pages/createNewArticle";
+import { articleContext } from "../context/articleContext";
+import { Simulate } from "react-dom/test-utils";
 
 describe("add article component", () => {
-  it("shows movies form", () => {
+  const user = {
+    microsoft: {
+      name: "Kai Johnson",
+    },
+  };
+
+  it("shows article form", () => {
     const element = document.createElement("div");
     const reload = jest.fn();
-    const user = {
-      name: "Kai Johnson",
-    };
 
     ReactDOM.render(
       <MemoryRouter>
@@ -23,5 +28,44 @@ describe("add article component", () => {
         (e) => e.innerHTML
       )
     ).toEqual(["Title:", "Topics:", "Author:"]);
+  });
+
+  it("adds article on submit", () => {
+    const createArticle = jest.fn();
+    const title = "Article1";
+    const element = document.createElement("div");
+    const reload = jest.fn();
+    ReactDOM.render(
+      <articleContext.Provider value={{ createArticle }}>
+        <MemoryRouter>
+          <CreateNewArticle user={user} reload={reload} />
+        </MemoryRouter>
+      </articleContext.Provider>,
+      element
+    );
+    Simulate.change(element.querySelector(".form-input input"), {
+      target: { value: title },
+    });
+
+    Simulate.change(element.querySelector(".form-input:nth-of-type(2) input"), {
+      target: { value: "topic1" },
+    });
+
+    Simulate.change(element.querySelector(".form-input:nth-of-type(3) input"), {
+      target: { value: "athor1" },
+    });
+
+    Simulate.change(element.querySelector("textarea"), {
+      target: { value: "text1" },
+    });
+
+    Simulate.submit(element.querySelector("form"));
+    expect(createArticle).toBeCalledWith({
+      title,
+      topic: "topic1",
+      author: "athor1",
+      text: "text1",
+      username: "Kai Johnson",
+    });
   });
 });
