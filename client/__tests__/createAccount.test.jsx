c2c45fa1a67b62515c058430b7331b19dea60ff4
mojibhashemi123 <muha006@student.kristiania.no>
2022-05-06 04:18:35 +0200

    adds account on submit

--- client/__tests__/createAccount.test.jsx
+++ client/__tests__/createAccount.test.jsx
@@ -2,6 +2,8 @@ import { CreateAccount } from "../pages/createAccount";
 import ReactDOM from "react-dom";
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
+import { Simulate } from "react-dom/test-utils";
+import { UserContext } from "../context/userContext";
 
 describe("add account component", () => {
   it("shows account form", () => {
@@ -18,5 +20,43 @@ describe("add account component", () => {
         (e) => e.innerHTML
       )
     ).toEqual(["First Name:", "Username:", "Email:", "Password:"]);
+  });
+
+  it("adds account on submit", () => {
+    const createAccount = jest.fn();
+    const name = "Samira";
+    const element = document.createElement("div");
+    const reload = jest.fn();
+    ReactDOM.render(
+      <UserContext.Provider value={{ createAccount }}>
+        <MemoryRouter>
+          <CreateAccount reload={reload} />
+        </MemoryRouter>
+      </UserContext.Provider>,
+      element
+    );
+    Simulate.change(element.querySelector(".form-input input"), {
+      target: { value: name },
+    });
+
+    Simulate.change(element.querySelector(".form-input:nth-of-type(2) input"), {
+      target: { value: "Samira123" },
+    });
+
+    Simulate.change(element.querySelector(".form-input:nth-of-type(3) input"), {
+      target: { value: "samira@hotmail.com" },
+    });
+
+    Simulate.change(element.querySelector(".form-input:nth-of-type(4) input"), {
+      target: { value: "password123" },
+    });
+
+    Simulate.submit(element.querySelector("form"));
+    expect(createAccount).toBeCalledWith({
+      name,
+      username: "Samira123",
+      email: "samira@hotmail.com",
+      password: "password123",
+    });
   });
 });
